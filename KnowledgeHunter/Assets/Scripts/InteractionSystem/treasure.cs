0c80acfbdb9753e3b20732ee2a1b168aad8852d7
Alexandre Costa <48624475+AlexCosta157@users.noreply.github.com>
2022-06-02 01:05:23 +0100

    inventory

---
+++ KnowledgeHunter/Assets/Scripts/InteractionSystem/treasure.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treasure : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _prompt;
+
+    public string InteractionPrompt => _prompt;
+
+    public bool Interact(Interactor interactor){
+       
+        var inventory = interactor.GetComponent<Inventory>();
+
+        if(inventory == null) return false;
+
+        if(inventory.HasKey){
+            Debug.Log("opening treasure");
+            return true;
+        } 
+
+        Debug.Log("No key found!");
+        return false;
+    }
+}
+
