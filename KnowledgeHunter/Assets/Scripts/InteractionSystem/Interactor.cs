3e451850049d1f7d011c771cfbe8929ffd0f55e2
Alexandre Costa <48624475+AlexCosta157@users.noreply.github.com>
2022-06-04 15:51:22 +0100

    Interaction System

--- KnowledgeHunter/Assets/Scripts/InteractionSystem/Interactor.cs
+++ KnowledgeHunter/Assets/Scripts/InteractionSystem/Interactor.cs
@@ -8,19 +8,29 @@ public class Interactor : MonoBehaviour
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
+    [SerializeField] private InteractionPrompt _interactionPromptUI;
 
     private readonly Collider[] _colliders = new Collider[3];
     [SerializeField] private int _numFound;
+
+    private IInteractable _interactable;
 
     private void Update(){
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
 
         if(_numFound > 0){
-            var interactable = _colliders[0].GetComponent<IInteractable>();
+            _interactable = _colliders[0].GetComponent<IInteractable>();
 
-            if(interactable != null && Input.GetKeyDown("e")){
-                interactable.Interact(this);
+            if(_interactable != null){
+                
+                if(!_interactionPromptUI.IsDisplayed) _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
+
+                if(Input.GetKeyDown("e")) _interactable.Interact(this);
             }
+        }
+        else{
+            if(_interactable != null) _interactable = null;
+            if(_interactionPromptUI.IsDisplayed) _interactionPromptUI.Close();
         }
     }
 
