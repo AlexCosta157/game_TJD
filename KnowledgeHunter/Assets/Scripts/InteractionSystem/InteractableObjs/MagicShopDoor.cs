db3ea407f90cde31b116ad926e2d198a008b63e9
Alexandre Costa <48624475+AlexCosta157@users.noreply.github.com>
2022-06-12 00:04:30 +0100

    new assets

---
+++ KnowledgeHunter/Assets/Scripts/InteractionSystem/InteractableObjs/MagicShopDoor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MagicShopDoor : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _prompt;
+
+    public string InteractionPrompt => _prompt;
+
+    public bool Interact(Interactor interactor){
+        //throw new System.NotImplementedException();
+        Debug.Log("opening MagicShop"); 
+        SceneManager.LoadScene(3);
+        return true;
+    }
+}
